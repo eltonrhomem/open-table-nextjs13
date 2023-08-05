71a0e0b82e5a52310840500906babca54b8ea422
Elton Rodrigues Homem <eltonrhomem@gmail.com>
2023-08-04 23:58:14 -0300

    refact: Refactoring all pages making components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,16 @@ import RestaurantCard from "./components/RestaurantCard"
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen px-10">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <NavBar />
-        {/* NAVBAR */}
-        <main>
-          {/* HEADER */}
-          <Header />
-          {/* HEADER */} 
-          {/* CARDS */}
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            {/* CARD */}
-              <RestaurantCard />
-            {/* CARD */}
-          </div>
-          {/* CARDS */}
-        </main>
-      </main>
+    <main className="bg-gray-100 min-h-screen w-screen">
+  <main className="max-w-screen-2xl m-auto bg-white">
+    <NavBar />
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        <RestaurantCard />
+      </div>
     </main>
+  </main>
+</main>
   )
 }
