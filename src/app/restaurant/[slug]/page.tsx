4b03e31ada02223f35e148b14e4d470b301f0abf
Elton Rodrigues Homem <eltonrhomem@gmail.com>
2023-08-06 20:42:41 -0300

    refact: refactoring pages and making Layouts to make a dryer code

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import NavBar from "@/app/components/NavBar"
-import Header from "./components/Header"
 import RestaurantNavBar from "./components/RestaurantNavBar"
 import Title from "./components/Title"
 import Rating from "./components/Rating"
@@ -9,44 +7,37 @@ import Reviews from "./components/Reviews"
 import ReservationCard from "./components/ReservationCard"
 
 export default function RestaurantDetails() {
-    return(<main className="bg-gray-100 min-h-screen w-screen">
-    <main className="max-w-screen-2xl m-auto bg-white">
-      {/* NAVBAR */}
-      <NavBar />
-      {/* NAVBAR */} 
-      {/* HEADER */}
-      <Header />
-      {/* HEADER */} 
-      {/* DESCRIPTION PORTION */}
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          {/* RESAURANT NAVBAR */}
-          <RestaurantNavBar />
-          {/* RESAURANT NAVBAR */} 
-          {/* TITLE */}
-         <Title />
-          {/* TITLE */} 
-          {/* RATING */}
-          <Rating />
-          {/* RATING */} 
-          {/* DESCRIPTION */}
-          <Description />
-          {/* DESCRIPTION */} 
-          {/* IMAGES */}
-          <Images />
-          {/* IMAGES */} 
-          {/* REVIEWS */}
-          <Reviews />
-          {/* REVIEWS */}
-        </div>
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
-        </div>
-      </div>
-      {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */} {/* RESERVATION
-      CARD PORTION */}
-    </main>
-  </main>
+    return(
+      <>
+          {/* DESCRIPTION PORTION */}
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+              {/* RESAURANT NAVBAR */}
+              <RestaurantNavBar />
+              {/* RESAURANT NAVBAR */} 
+              {/* TITLE */}
+              <Title />
+              {/* TITLE */} 
+              {/* RATING */}
+              <Rating />
+              {/* RATING */} 
+              {/* DESCRIPTION */}
+              <Description />
+              {/* DESCRIPTION */} 
+              {/* IMAGES */}
+              <Images />
+              {/* IMAGES */} 
+              {/* REVIEWS */}
+              <Reviews />
+              {/* REVIEWS */}
+            </div>
+            <div className="w-[27%] relative text-reg">
+              <ReservationCard />
+            </div>
+      </>
+           
+               
+           
+            
   
   )
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,24 +4,19 @@ import RestaurantCard from "./components/RestaurantCard"
 import SearchSideBar from "./components/SearchSideBar"
 
 export default function Search() {
-    return(<main className="bg-gray-100 min-h-screen w-screen">
-    <main className="max-w-screen-2xl m-auto bg-white">
-      {/* NAVBAR */}
-      <NavBar />
-      {/* HEADER */}
-      <Header />
-      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        {/* SEARCH SIDE BAR */}
-        <SearchSideBar />
-        {/* SEARCH SIDE BAR */}
-        <div className="w-5/6">
-          {/* RESAURANT CARD */}
-          <RestaurantCard />
-          {/* RESAURANT CAR */}
-        </div>
-      </div>
-    </main>
-  </main>
-  
-  )
+    return(
+              <>
+                <Header />
+                <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+                    {/* SEARCH SIDE BAR */}
+                    <SearchSideBar />
+                    {/* SEARCH SIDE BAR */}
+                    <div className="w-5/6">
+                      {/* RESAURANT CARD */}
+                      <RestaurantCard />
+                      {/* RESAURANT CAR */}
+                    </div>
+                </div>
+              </>
+          )
 }
